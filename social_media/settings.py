5f0f2c270ec2a2b23d09ac575cc68ecebc461357
Obeng JOsiah <josiahobeng87@gmail.com>
2025-09-07 13:01:14 -0700

    little adjuscement on settings url

--- social_media/settings.py
+++ social_media/settings.py
@@ -16,8 +16,7 @@ SECRET_KEY = "your-secret-key"
 # SECURITY WARNING: don’t run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =  ALLOWED_HOSTS = ['obeng124.pythonanywhere.com', '127.0.0.1', 'localhost']
-
+ALLOWED_HOSTS =  ALLOWED_HOSTS = ['obeng124.pythonanywhere.com']
 
 
 
